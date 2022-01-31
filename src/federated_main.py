d9fe089611a686bf62a5987f951270980bdc47e8
user <user@Mac1980.amesiaaa.metronetinc.net>
2022-01-31 01:46:28 -0600

    Fix pickling path, gitignore '.pkl' files

--- src/federated_main.py
+++ src/federated_main.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     print("|---- Test Accuracy: {:.2f}%".format(100*test_acc))
 
     # Saving the objects train_loss and train_accuracy:
-    file_name = '../save/objects/{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}].pkl'.\
+    file_name = './save/objects/{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}].pkl'.\
         format(args.dataset, args.model, args.epochs, args.frac, args.iid,
                args.local_ep, args.local_bs)
 
